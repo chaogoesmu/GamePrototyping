d6ad49f5306f2d8313ab17b4557ac005bd74501a
chaogoesmu <chaogoesmu@gmail.com>
2019-04-30 09:41:27 -0700

    added some wander code
    start of wander code to get a random point to go to.

---
+++ New Unity Project/Assets/Scripts/Wander.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+
+public class Wander : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = RandomNavSphere(myAgent.transform.position, wanderRadius, -1);
+        agent.SetDestination(newPos);
+    }
+
+    /// <summary>
+    /// find a random point on the layermask provided that is "dist" far away from the origin
+    /// </summary>
+    /// <param name="origin"></param>
+    /// <param name="dist"></param>
+    /// <param name="layermask"></param>
+    /// <returns></returns>
+    public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
+    {
+        Vector3 randDirection = Random.insideUnitSphere * dist;
+
+        randDirection += origin;
+
+        UnityEngine.AI.NavMeshHit navHit;
+
+        UnityEngine.AI.NavMesh.SamplePosition(randDirection, out navHit, dist, layermask);
+
+        return navHit.position;
+    }
+}
