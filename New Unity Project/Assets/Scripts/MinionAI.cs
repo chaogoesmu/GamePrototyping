008ba7684cc064eb23ec696578d25db49c95c099
chaogoesmu <chaogoesmu@gmail.com>
2019-05-06 15:17:38 -0700

    ai now a state machine
    made it so it can do seperate actions, added idle, busy, etc.

--- New Unity Project/Assets/Scripts/MinionAI.cs
+++ New Unity Project/Assets/Scripts/MinionAI.cs
@@ -7,32 +7,73 @@ public class MinionAI : MonoBehaviour
 
     private UnityEngine.AI.NavMeshAgent agent; //= GetComponent<UnityEngine.AI.NavMeshAgent>();
     public float wanderRadius;
-    public enum Actions { idle, wander }
+    public enum Actions { undecided, idle, wander, busy }
     public Actions currentAction;
+    private int ActionsLength = 4;
+    public float idleTimeMax = 5f;
+    public float ifleTimeMin = 15f;
     // Start is called before the first frame update
     void Start()
     {
         agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
-        if (agent == null)
+        if (agent != null)
         {
-
             currentAction = Actions.wander;
-            Debug.Log("current action: "+(int)currentAction + " " + currentAction);
-            Debug.Log("agent is null, something went wrong");
+            Debug.Log("current action: "+currentAction + " " + currentAction);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ((int)currentAction == 1)
+        switch (currentAction)
         {
-            wandering();
+            case Actions.wander:
+                wandering();
+                break;
+            case Actions.idle:
+                StartCoroutine(idle(Random.Range(idleTimeMax, ifleTimeMin)));
+                break;
+            case Actions.busy:
+                break;
+            case Actions.undecided:
+                Debug.Log("deciding action");
+                currentAction = Actions.busy;
+                while (currentAction == Actions.busy)
+                {
+                    //var val = Enum.GetNames(Actions);
+                    currentAction = (Actions)Mathf.Floor(Random.value*ActionsLength);
+                    Debug.Log("new action: " + currentAction);
+                }
+                
+                break;
+            default:
+                Debug.Log("no action taken: "+ currentAction);
+                break;
         }
-        else { Debug.Log("action is idle, something went wrong."); }
+    }
+
+    private IEnumerator idle(float time)
+    {
+        Debug.Log("idling for: " + time);
+        currentAction = Actions.busy;
+        yield return new WaitForSeconds((int)time);
+
+        // Code to execute after the delay
+        //idle animation
+        currentAction = Actions.undecided;
     }
     private void wandering()
     {
+        if (agent.isStopped)
+        {
+            Vector3 newPos = ForwardRandomNavSphere(this.transform.position + transform.forward * (wanderRadius / 3), wanderRadius, -1);
+            agent.destination = newPos;
+            Debug.Log(this + " changed destination to: " + agent.destination);
+            Debug.DrawRay(agent.destination, Vector3.up * 30, Color.blue, 5.0f);
+            agent.isStopped = false;
+            currentAction = Actions.wander;
+        }
         if (!agent.pathPending)//boolean on whether it has a path to follow
         {
 
@@ -44,10 +85,9 @@ public class MinionAI : MonoBehaviour
                 {
                     Debug.Log(this + "No path currently" + agent.destination);
                 }
-                Vector3 newPos = ForwardRandomNavSphere(this.transform.position + transform.forward * (wanderRadius / 3), wanderRadius, -1);
-                agent.destination = newPos;
-                Debug.Log(this + " changed destination to: " + agent.destination);
-                Debug.DrawRay(agent.destination, Vector3.up * 30, Color.blue, 1.0f);
+                agent.isStopped = true;
+                GetComponent<Rigidbody>().velocity = Vector3.zero; //agent was drifting occasionally after attempting to stop.
+                currentAction = Actions.undecided;
             }
             return;
         }
