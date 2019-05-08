cd3fd79995f15e7a33359d28a4d6f823bc7c0f3c
chaogoesmu <chaogoesmu@gmail.com>
2019-05-08 09:25:16 -0700

    Update TreeScript.cs
    changed it so that the tree gathers energy, then grows when full of energy.

--- New Unity Project/Assets/Scripts/TreeScript.cs
+++ New Unity Project/Assets/Scripts/TreeScript.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class TreeScript : MonoBehaviour
 {
     public float growth = 0;
+    public float energy = 0;
+    public float maxEnergy = 100f;
+    public float growthScaling = .1f;
+    public float energyRate = .1f;
     public float maxSize = 100f;
     public float growthRate = .05f;
     // Start is called before the first frame update
@@ -16,14 +20,19 @@ public class TreeScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        growth += Time.deltaTime * growthRate;
-        if (growth >= maxSize)
+        energy+= Time.deltaTime * energyRate;//tree gathers energy, when it's full on energy it grows
+        if (energy >= maxEnergy)
         {
+            energy = 0;
+            growth += growthRate;
+            if (growth >= maxSize)//if a tree is fully grown, it spawns
+            {
+                Vector3 randPoint = MinionAI.ForwardRandomNavSphere(this.transform.position, 20, -1);
 
-            Vector3 randPoint = MinionAI.ForwardRandomNavSphere(this.transform.position, 20,-1);
-            //spawn a tree
-            Debug.DrawRay(randPoint, Vector3.up * 30, Color.green, 5.0f);
-            growth = 0f;
+                Debug.DrawRay(randPoint, Vector3.up * 30, Color.green, 5.0f);
+                //TODO:spawn a tree
+                //TODO: create a tree with an origin point at the bottom, may need to attach it to a blank gameobject
+            }
         }
     }
 
